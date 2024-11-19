febf1957b8ed61cf36a5f3cd1440f664619a4704
Nepomuk Crhonek <105591323+Nepomuk5665@users.noreply.github.com>
2024-11-19 08:02:14 +0100

    dir chnage

--- Aufgaben/Nepomuk/01_NotenDurchschnittRechner/src/Main.java
+++ Aufgaben/Nepomuk/01_NotenDurchschnittRechner/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Willkommen zum Schweizer Noten rechner!\nHow many tests?(minimum 3)");
+
+        Scanner input = new Scanner(System.in); // Scanner Objekt
+
+        int numberofTests = input.nextInt();
+
+        
+
+
+
+
+
     }
 }
