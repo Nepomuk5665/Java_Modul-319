93b3a6f52ab469c9a5e2946c90d21d022f44111b
dgdecorso <diego.decorso@edu.tbz.ch>
2025-01-28 11:18:18 +0100

    Add files via upload

---
+++ Aufgaben/Diego/Arcade/src/ch/noseryoung/blj/Text.java
@@ -0,0 +1,16 @@
+package ch.noseryoung.blj;
+
+public class Text {
+    public static void textArcade() {
+        System.out.println("Welcome to Arcade");
+        System.out.println(" ______     ______     ______     ______     _____     ______    ");
+        System.out.println("/\\  __ \\   /\\  == \\   /\\  ___\\   /\\  __ \\   /\\  __-.  /\\  ___\\   ");
+        System.out.println("\\ \\  __ \\  \\ \\  __<   \\ \\ \\____  \\ \\  __ \\  \\ \\ \\/\\ \\ \\ \\  __\\   ");
+        System.out.println(" \\ \\_\\ \\_\\  \\ \\_\\ \\_\\  \\ \\_____\\  \\ \\_\\ \\_\\  \\ \\____-  \\ \\_____\\ ");
+        System.out.println("  \\/_/\\/_/   \\/_/ /_/   \\/_____/   \\/_/\\/_/   \\/____/   \\/_____/ ");
+        System.out.println("                                                                 ");
+
+    }
+
+
+}
